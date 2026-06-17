052ab1c75452489a02f92a7f80c4d037067de8e4
sambytemail-max <sambyte.mail@gmail.com>
2026-06-17 15:06:34 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById("modal");
+const openModal = document.getElementById("openModal");
+const closeModal = document.getElementById("closeModal");
+
+openModal.onclick = function () {
+  modal.style.display = "block";
+};
+
+closeModal.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+};
